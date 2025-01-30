f71f1d514e7919032d44c64556d2f5c0a12b10ff
wizgurl101 <nb.phan@hotmail.com>
2025-01-29 22:58:40 -0700

    add models

---
+++ models/ollama.go
@@ -0,0 +1,12 @@
+package models
+
+type OllamaRequest struct {
+	Model    string    `json:"model"`
+	Messages []Message `json:"messages"`
+	Stream   bool      `json:"stream"`
+}
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
