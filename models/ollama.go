8b434e476c9f862332c5487af51b32beaac28d32
wizgurl101 <nb.phan@hotmail.com>
2025-01-29 23:10:30 -0700

    add response struct

--- models/ollama.go
+++ models/ollama.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type OllamaRequest struct {
 	Model    string    `json:"model"`
 	Messages []Message `json:"messages"`
@@ -9,4 +11,17 @@ type OllamaRequest struct {
 type Message struct {
 	Role    string `json:"role"`
 	Content string `json:"content"`
+}
+
+type OllamaResponse struct {
+	Model              string    `json:"model"`
+	CreatedAt          time.Time `json:"created_at"`
+	Message            Message   `json:"message"`
+	Done               bool      `json:"done"`
+	TotalDuration      int64     `json:"total_duration"`
+	LoadDuration       int       `json:"load_duration"`
+	PromptEvalCount    int       `json:"prompt_eval_count"`
+	PromptEvalDuration int       `json:"prompt_eval_duration"`
+	EvalCount          int       `json:"eval_count"`
+	EvalDuration       int64     `json:"eval_duration"`
 }
